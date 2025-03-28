0a2112fa962fdea4ea7b505be11bdb82bfd24ac3
ctnsupra1 <ctnsupra1@hotmail.com>
2025-03-28 07:15:44 -0400

    Ignore test files from future commits

---
+++ resources/views/custom-head.blade.php
@@ -0,0 +1,12 @@
+<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
+
+<style>
+  .header .logo-text,
+  .header-title,
+  header .text,
+  .header .header-logo,
+  .header {
+    font-family: 'Orbitron', sans-serif !important;
+  }
+</style>
+
---
+++ resources/views/custom-head.blade.phpt
@@ -0,0 +1,12 @@
+<style>
+  @font-face {
+    font-family: 'Microgramma';
+    src: url('/fonts/Microgramma.otf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  header .logo-text, .header-title {
+    font-family: 'Microgramma', sans-serif !important;
+  }
+</style>
